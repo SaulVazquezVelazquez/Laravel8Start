d4a4887e88b407f9f62d37420212fe4d24bcecc3
Saul Vazquez Velazquez <saul.vazquez@gmail.com>
2022-02-19 20:15:17 -0600

    Proyecto Iniciado Laravel 8

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Home Admin')
+
+@section('content_header')
+    <h1>Home Admin</h1>
+@stop
+
+@section('content')
+    <p>Home Admin.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
---
+++ routes/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Admin\Home\HomeController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::resource('', HomeController::class)->names('admin.home.index');
+
+// Route::get('',function()
+// {
+//     return "Hola Adminsitrador";
+// });
